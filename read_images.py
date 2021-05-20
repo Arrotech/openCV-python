9c0ab363d28a27c58611644dfd74911a4ba15121
Harun Gachanja <arrotechdesign@gmail.com>
2021-05-20 11:50:06 +0300

    Feature (Resize Images and Videos)
    What does this PR do?
    
    Resize images and videos with openCV.

--- read_images.py
+++ read_images.py
@@ -1,6 +1,20 @@
 import cv2 as cv
 
 img = cv.imread('images/ppic.jpg')
+
+def resizeframe(frame, scale=0.75):
+    """Resisze the frame of the image."""
+    width = int(frame.shape[1] * scale)
+    height = int(frame.shape[0] * scale)
+
+    dimensions = (width, height)
+
+    return cv.resize(frame, dimensions, interpolation=cv.INTER_AREA)
+
+resized_img = resizeframe(img)
+
 cv.imshow('Profile Picture', img)
+cv.imshow('Resized Profile Picture', resized_img)
+
 
 cv.waitKey(0)
